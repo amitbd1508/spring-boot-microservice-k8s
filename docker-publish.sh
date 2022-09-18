c7a04202f5e65d0e43e080088a22e5ee0f0807a4
Amit <amit.ghosh@cefalo.com>
2022-09-18 01:25:19 -0500

    add shell file and add k8s profile

---
+++ docker-publish.sh
@@ -0,0 +1,6 @@
+
+docker build -t amitbd1508/account-service:1.0 .
+docker build -t amitbd1508/order-service:1.0 .
+docker build -t amitbd1508/product-service:1.0 .
+docker build -t amitbd1508/payemnt-service:1.0 .
+
